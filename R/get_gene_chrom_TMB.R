7632c546fb9e8292d73ee774df2756f9cc3a995b
Axel Martin <axel.ste.martin@gmail.com>
2019-11-13 15:39:10 -0500

    Gene specific chromatin

---
+++ R/get_gene_chrom_TMB.R
@@ -0,0 +1,59 @@
+#' get_gene_chrom_TMB
+#'
+#' Combines bed files with WGS/WXS/IMPACT genes
+#' for chromatin accessibility and TMB
+#'
+#' @param bed BED file with fully processed with 7 columns
+#' ("Chromosome","start","end","type","score","strand","signal").
+#' @param gen complete maf file with chromosome, chromosome start/end, sample ID
+#' @param map Start and end positions of all genes in the genome (or interest)
+#' @param seg.size Size of segments to be used.
+#' @return dat a data frame with the name of genes, their positions and their
+#' estimated chromatin access and TMB.
+#'
+#'@export
+#'
+
+get_gene_chrom_TMB <- function(bed,gen,map,seg.size){
+
+  # get the equivalent segments for the gene mapping #
+  map <- map %>%
+    mutate(start.mb = trunc(as.numeric(as.character(start))/seg.size),
+           end.mb = trunc(as.numeric(as.character(end))/seg.size))
+
+  final <- data.table(do.call('rbind',lapply(c(1:22,"X"),function(x){
+    print(x)
+
+    # sub map #
+    map.sub <- map %>%
+      filter(chrom == paste0("chr",x))
+
+    gen.sub <- gen %>%
+      filter(chromosome == x)
+
+    # subset to that chromosome #
+    bed.sub <- bed %>%
+      filter(Chromosome == paste0("chr",x)) %>%
+      select(start,end,signal) %>%
+      mutate_all(as.numeric)
+
+    info <- as.data.frame(t(apply(map.sub[1:2,],1,function(y){
+      range <- c(as.numeric(y[3])-seg.size,as.numeric(y[4])+seg.size)
+      mean.bed <- mean(unlist(bed.sub %>%
+                                filter(start >= range[1], end <= range[2]) %>%
+                                select(signal)))
+      tmb <- mean(unlist(gen.sub %>%
+                    filter(chromosome_start >= range[1], chromosome_end <= range[2]) %>%
+                    group_by(as.character(submitted_sample_id)) %>%
+                    summarise(N = n()) %>%
+                    select(N)))
+
+      out <- c(unlist(y),mean.bed,tmb)
+      out[3:6] <- as.numeric(out[3:6])
+      return(out)
+    })))
+    colnames(info)[5:6] <- c("ChromAccess","TMB")
+  return(info)
+  })))
+return(final)
+}
