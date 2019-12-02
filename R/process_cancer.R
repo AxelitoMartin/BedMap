b69a116e9a3827627e3174891aa98e092dddeaca
Axel Martin <axel.ste.martin@gmail.com>
2019-12-02 18:28:43 -0500

    Update process_cancer.R

--- R/process_cancer.R
+++ R/process_cancer.R
@@ -89,10 +89,12 @@ process_cancer <- function(cancer,path,output.path){
     })
   }
   if(length(files) == 1){
-    temp <- first
+    temp <- first %>%
+      select(chrom,Position,paste0("file",1:length(files)))
     colnames(temp)[ncol(temp)] <- "H4K3me1"
   }
   print(colnames(temp))
+  print(head(temp))
   H3K4me1 <- temp %>% select(chrom,Position,H3K4me1)
 
   if(is.null(Chrom)) fulldat <- H3K4me1
